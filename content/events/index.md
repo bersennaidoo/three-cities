d27df5950da139b15061fcb4e43af55f87fe64fc
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-08-05 15:31:33 +0200

    Setup react and custom template

---
+++ content/events/index.md
@@ -0,0 +1,6 @@
+---
+title: 'Events'
+date: 2024-08-05T15:22:42+02:00
+draft: false
+layout: events
+---
---
+++ static/react/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+function App() {
+  return (
+    <div>
+        <h1>Hello From Hugo React</h1>
+    </div>
+  )
+}
+
+export default App
