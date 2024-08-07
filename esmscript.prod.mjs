db48e5c1190da874554d3b7170b921f74cdf01ef
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-08-07 11:55:58 +0200

    Updated esbuild config

--- esmscript.prod.mjs
+++ esmscript.prod.mjs
@@ -7,5 +7,8 @@ await esbuild.build({
   sourcemap: true,
   loader: { '.png': 'file', '.jpg': 'file' },
   publicPath: 'https://three-cities.netlify.app/react',
-  outdir: 'static/react',
+  outdir: 'static/react', 
+  define: {
+      'process.env.VITE_CLERK_PUBLISHABLE_KEY': '"pk_test_cG9zc2libGUtbWFjYXctOS5jbGVyay5hY2NvdW50cy5kZXYk"',
+  },
 })
